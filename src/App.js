e1833ac404f3c5735ca3662329751d9d174048ea
Geeta <gtturumella@gmail.com>
2023-05-07 23:32:15 -0400

    lesson-1-1 submission

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from 'react';
 
+const todoList = [
+  {
+    Id:1,
+    Title:"Pre-made Flower Basket:\n",
+    },
+     {
+     Id:2,
+    Title:"Selection of Multicolor Flowers:\n",
+    },
+     {
+    Id:3,
+    Title:"Speciality Rose Bouquets:\n",
+    },
+    {
+    Id:4,
+      Title:"Design Your Own Floral Arrangement:\n",
+      },
+    ];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+        <h1>TrendyFloristApp</h1>
+        <h2>Todo List</h2>
+        </header>
+        <ul>
+        {todoList.map(function(item){
+               return (
+               <li key = {item.Id}>
+                <span>{item.Title}</span>
+          
+
+        
+      
+
+      </li>
+               );
+        })}    
+           </ul>       
+      
+      
+      
     </div>
   );
 }
