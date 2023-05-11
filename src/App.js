389b37f7d9fe6d659e89508fe741c12388346836
Geeta <gtturumella@gmail.com>
2023-05-10 20:45:04 -0400

    Lesson-1-3 Submission

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import  React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
+import {useState} from 'react';
+import TodoListItem from './TodoListItem';
+
 
 function App() {
-  return (
-    <div>
-      <header>
-        <h1>TrendyFloristApp</h1>
-        <h2>Todo List</h2>
-        </header>
-         <TodoList/>
-         <AddTodoForm/>
-    </div>
+ const [newTodo, setNewTodo] = useState('');
+ return (
+   <div>
+     <header>
+     <h1>Trendyfloristapp</h1>
+       <h2>Todo List</h2>
+       </header>
+
+     <TodoList/>
+     <TodoListItem/>
+
+     <AddTodoForm onAddTodo={(todoTitle)=> setNewTodo(todoTitle)} />
+        <p>
+         Displaying the Value of <strong>{newTodo}</strong>
+        </p>
+            </div>
   );
 }
 
